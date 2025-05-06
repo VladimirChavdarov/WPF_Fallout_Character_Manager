e726c6e464c24d19dcbc3283d4762af558287dbd
Vladimir Chavdarov <221716@buas.nl>
2025-05-06 23:38:09 +0200

    🏁Uploaded project

---
+++ WPF_Fallout_Character_Manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Fallout_Character_Manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
