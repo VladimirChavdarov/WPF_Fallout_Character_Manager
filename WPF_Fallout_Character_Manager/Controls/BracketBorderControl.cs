4c13e99e16a1fb560877e3a21d4cc5aea0dbfb85
Vladimir Chavdarov <221716@buas.nl>
2025-05-09 02:56:37 +0200

    📦 Made the grid for all panels in the "Character page". TODO: Tab/Page bar next
    Note: I also tried playing around with the border styles, tried to make a "[ ]" style but got tangled in binding properties. Maybe worth revisiting later.

---
+++ WPF_Fallout_Character_Manager/Controls/BracketBorderControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Fallout_Character_Manager.Controls
+{
+    class BracketBorderControl : ContentControl
+    {
+        public static readonly DependencyProperty BrushThicknessProperty =
+            DependencyProperty.Register(
+                "BrushThickness",
+                typeof(double),
+                typeof(BracketBorderControl),
+                new PropertyMetadata(1.0)
+                );
+
+        public double BrushThickness
+        {
+            get => (double)GetValue( BrushThicknessProperty );
+            set => SetValue( BrushThicknessProperty, value );
+        }
+
+        static BracketBorderControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BracketBorderControl), new FrameworkPropertyMetadata(typeof(BracketBorderControl)));
+        }
+    }
+}
