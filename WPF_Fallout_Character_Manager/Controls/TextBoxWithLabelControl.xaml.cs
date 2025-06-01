24635ac73ad8014810abb985271fcca8e88bea39
Vladimir Chavdarov <221716@buas.nl>
2025-06-02 00:53:42 +0200

    Made a UserControl that combines ChunkyTextBox with a Label

---
+++ WPF_Fallout_Character_Manager/Controls/TextBoxWithLabelControl.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Fallout_Character_Manager.Models.ModifierSystem;
+
+namespace WPF_Fallout_Character_Manager.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxWithLabelControl.xaml
+    /// </summary>
+    public partial class TextBoxWithLabelControl : UserControl
+    {
+        //Dependency Properties
+        #region Label
+        public static readonly DependencyProperty LabelTextProperty =
+        DependencyProperty.Register("LabelText", typeof(string), typeof(TextBoxWithLabelControl),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnLabelTextChanged)));
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        private static void OnLabelTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextBoxWithLabelControl? ThisUserControl = d as TextBoxWithLabelControl;
+            ThisUserControl.OnLabelTextChanged(e);
+        }
+
+        private void OnLabelTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            // This overrides the field and doesn't allow a new value to appear if the Model changed.
+            //CustomTextBox.Text = e.NewValue.ToString();
+        }
+        #endregion
+
+        #region TextBox
+        public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(TextBoxWithLabelControl),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnTextChanged)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextBoxWithLabelControl? ThisUserControl = d as TextBoxWithLabelControl;
+            ThisUserControl.OnTextChanged(e);
+        }
+
+        private void OnTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            // This overrides the field and doesn't allow a new value to appear if the Model changed.
+            //CustomTextBox.Text = e.NewValue.ToString();
+        }
+        #endregion
+
+        #region ModInt
+        public static readonly DependencyProperty ModIntProperty =
+            DependencyProperty.Register("ModInt", typeof(ModInt), typeof(TextBoxWithLabelControl),
+                new FrameworkPropertyMetadata(null, new PropertyChangedCallback(ModIntPropertyChanged)));
+
+        public ModInt ModInt
+        {
+            get => (ModInt)GetValue(ModIntProperty);
+            set => SetValue(ModIntProperty, value);
+        }
+
+        private static void ModIntPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextBoxWithLabelControl? ThisUserControl = d as TextBoxWithLabelControl;
+            ThisUserControl.ModIntPropertyChanged(e);
+        }
+
+        private void ModIntPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Orientation
+        public static readonly DependencyProperty OrientationProperty =
+        DependencyProperty.Register("Orientation", typeof(Orientation), typeof(TextBoxWithLabelControl),
+            new FrameworkPropertyMetadata(Orientation.Horizontal, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnOrientationChanged)));
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        private static void OnOrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextBoxWithLabelControl? ThisUserControl = d as TextBoxWithLabelControl;
+            ThisUserControl.OnOrientationChanged(e);
+        }
+
+        private void OnOrientationChanged(DependencyPropertyChangedEventArgs e)
+        {
+            // This overrides the field and doesn't allow a new value to appear if the Model changed.
+            //CustomTextBox.Text = e.NewValue.ToString();
+        }
+        #endregion
+        //
+
+        public TextBoxWithLabelControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
