dcd9a0e8370509e3521733c2c2f570529e6d082a
Vladimir Chavdarov <221716@buas.nl>
2025-06-09 22:16:38 +0200

    Created a new UpDownTextBox and Fixed BioModel with proper encapsulation

---
+++ WPF_Fallout_Character_Manager/Controls/UpDownTextBoxControl.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Fallout_Character_Manager.Models.ModifierSystem;
+
+namespace WPF_Fallout_Character_Manager.Controls
+{
+    /// <summary>
+    /// Interaction logic for UpDownTextBoxControl.xaml
+    /// </summary>
+    public partial class UpDownTextBoxControl : UserControl
+    {
+        // Dependency Properties
+        #region TextBox
+        public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(UpDownTextBoxControl),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnTextChanged)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UpDownTextBoxControl? ThisUserControl = d as UpDownTextBoxControl;
+            ThisUserControl.OnTextChanged(e);
+        }
+
+        private void OnTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            // This overrides the field and doesn't allow a new value to appear if the Model changed.
+            //CustomTextBox.Text = e.NewValue.ToString();
+        }
+        #endregion
+
+        #region ModInt
+        public static readonly DependencyProperty ModIntProperty =
+            DependencyProperty.Register("ModInt", typeof(ModInt), typeof(UpDownTextBoxControl),
+                new FrameworkPropertyMetadata(null, new PropertyChangedCallback(ModIntPropertyChanged)));
+
+        public ModInt ModInt
+        {
+            get => (ModInt)GetValue(ModIntProperty);
+            set => SetValue(ModIntProperty, value);
+        }
+
+        private static void ModIntPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UpDownTextBoxControl? ThisUserControl = d as UpDownTextBoxControl;
+            ThisUserControl.ModIntPropertyChanged(e);
+        }
+
+        private void ModIntPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        #endregion
+        //
+
+        public UpDownTextBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Text, out int value))
+            {
+                value--;
+                Text = value.ToString();
+            }
+            else
+            {
+                Text = "0";
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Text, out int value))
+            {
+                value++;
+                Text = value.ToString();
+            }
+            else
+            {
+                Text = "0";
+            }
+        }
+    }
+}
--- WPF_Fallout_Character_Manager/Models/BioModel.cs
+++ WPF_Fallout_Character_Manager/Models/BioModel.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_Fallout_Character_Manager.Models.ModifierSystem;
+using WPF_Fallout_Character_Manager.Models.MVVM;
 
 namespace WPF_Fallout_Character_Manager.Models
 {
-    internal sealed class BioModel
+    internal sealed class BioModel : ModelBase
     {
         public BioModel()
         {
@@ -20,12 +22,53 @@ namespace WPF_Fallout_Character_Manager.Models
             ImageSource = "Resources/Vault_Boy.png";
         }
 
-        public string Name { get; set; }
-        public string Race { get; set; }
-        public string Background { get; set; }
-        public string Backstory { get; set; }
-        public int Level { get; set; }
-        public int XP { get; set; }
-        public string ImageSource { get; set; }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => Update(ref _name, value);
+        }
+
+        private string _race;
+        public string Race
+        {
+            get => _race;
+            set => Update(ref _race, value);
+        }
+
+        private string _background;
+        public string Background
+        {
+            get => _background;
+            set => Update(ref _background, value);
+        }
+
+        private string _backstory;
+        public string Backstory
+        {
+            get => _backstory;
+            set => Update(ref _backstory, value);
+        }
+
+        private int _level;
+        public int Level
+        {
+            get => _level;
+            set => Update(ref _level, value);
+        }
+
+        private int _xp;
+        public int XP
+        {
+            get => _xp;
+            set => Update(ref _xp, value);
+        }
+
+        private string _imageSource;
+        public string ImageSource
+        {
+            get => _imageSource;
+            set => Update(ref _imageSource, value);
+        }
     }
 }
